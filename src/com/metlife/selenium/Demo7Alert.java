25cb7ca73e02c97e81acc765d709ca772c6ef545
balaji-githubstore <balaji0017@gmail.com>
2025-11-07 17:37:07 +0530

    Alert Code with Explicit wait

--- src/com/metlife/selenium/Demo7Alert.java
+++ src/com/metlife/selenium/Demo7Alert.java
@@ -1,7 +1,10 @@
 package com.metlife.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -11,12 +14,20 @@ public class Demo7Alert {
 
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         driver.get("https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm");
 
         //click on Go
+        //driver.findElement(By.xpath("//img[@alt='Go']")).click();
 
+        //wait for alert present - explicit wait
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.alertIsPresent());
 
+        String actualAlertMessage= driver.switchTo().alert().getText();
+        System.out.println(actualAlertMessage);
+
+        driver.switchTo().alert().accept();
     }
 }
