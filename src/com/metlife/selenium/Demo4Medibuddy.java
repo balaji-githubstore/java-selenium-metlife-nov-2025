7382ed12b3b8d94182bc70e2cf2eac5b506fce04
balaji-githubstore <balaji0017@gmail.com>
2025-11-07 14:35:03 +0530

    Complete Meddibuddy

---
+++ src/com/metlife/selenium/Demo4Medibuddy.java
@@ -0,0 +1,19 @@
+package com.metlife.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo4Medibuddy {
+
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.medibuddy.in/");
+
+        //click on login
+
+        //enter phone number as 787887878
+
+
+    }
+}
