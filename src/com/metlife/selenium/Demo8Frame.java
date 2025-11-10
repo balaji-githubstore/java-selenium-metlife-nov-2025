1745d787ca7a8b5b2eeb806387323b5504a58dc5
balaji-githubstore <balaji0017@gmail.com>
2025-11-10 14:36:11 +0530

    Frame concepts

--- src/com/metlife/selenium/Demo8Frame.java
+++ src/com/metlife/selenium/Demo8Frame.java
@@ -1,12 +1,19 @@
 package com.metlife.selenium;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Demo8Frame {
 
     public static void main(String[] args) {
 
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        driver.get("https://netbanking.hdfcbank.com/netbanking/");
 
     }
 }
