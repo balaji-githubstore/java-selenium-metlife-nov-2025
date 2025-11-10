d375f17e108f713b2682e666676f02f47501164c
balaji-githubstore <balaji0017@gmail.com>
2025-11-10 15:46:14 +0530

    Frame concepts and Xpath using normalize-space

--- src/com/metlife/selenium/Demo8Frame.java
+++ src/com/metlife/selenium/Demo8Frame.java
@@ -1,5 +1,6 @@
 package com.metlife.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,12 +9,28 @@ import java.time.Duration;
 public class Demo8Frame {
 
     public static void main(String[] args) {
-
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         driver.get("https://netbanking.hdfcbank.com/netbanking/");
+
+        //switch using WebElement
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='login_page']")));
+
+        //switch using index
+//        driver.switchTo().frame(0);
+
+        //switch using name or id as String
+//        driver.switchTo().frame("login_page");
+
+        driver.findElement(By.name("fldLoginUserId")).sendKeys("john123");
+
+        //click on Continue
+        driver.findElement(By.linkText("CONTINUE")).click();
+
+        //switch to main html
+        driver.switchTo().defaultContent();
 
     }
 }
